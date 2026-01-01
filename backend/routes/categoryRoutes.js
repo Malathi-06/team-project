17126e637aab2633282650c708af99ad0f50a3cc
amirtha-1412 <kit27.am006@gmail.com>
2026-01-01 17:01:39 +0530

    API Tested

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -7,14 +7,15 @@ const {
     updateCategory,
     deleteCategory,
 } = require("../controllers/categoryController");
+const { protect, admin } = require("../middleware/authMiddleware");
 
 // @route   GET /api/categories
 // @route   POST /api/categories
-router.route("/").get(getCategories).post(createCategory);
+router.route("/").get(getCategories).post(protect, admin, createCategory);
 
 // @route   GET /api/categories/:id
 // @route   PUT /api/categories/:id
 // @route   DELETE /api/categories/:id
-router.route("/:id").get(getCategoryById).put(updateCategory).delete(deleteCategory);
+router.route("/:id").get(getCategoryById).put(protect, admin, updateCategory).delete(protect, admin, deleteCategory);
 
 module.exports = router;
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -7,14 +7,15 @@ const {
     updateProduct,
     deleteProduct,
 } = require("../controllers/productController");
+const { protect, admin } = require("../middleware/authMiddleware");
 
 // @route   GET /api/products
 // @route   POST /api/products
-router.route("/").get(getProducts).post(createProduct);
+router.route("/").get(getProducts).post(protect, createProduct);
 
 // @route   GET /api/products/:id
 // @route   PUT /api/products/:id
 // @route   DELETE /api/products/:id
-router.route("/:id").get(getProductById).put(updateProduct).delete(deleteProduct);
+router.route("/:id").get(getProductById).put(protect, admin, updateProduct).delete(protect, admin, deleteProduct);
 
 module.exports = router;
